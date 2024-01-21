bef1a53fd47b402efbe06789b3a8830a9f5b2ec8
pkufool <wkang@pku.org.cn>
2024-01-21 17:02:47 +0800

    more scripts

---
+++ install.sh
@@ -0,0 +1,33 @@
+
+
+
+if [ ! d ~/dev_tools/nvim-linux64 ]; then
+  wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-linux64.tar.gz -O ~/dev_tools/nvim-linux64.tar.gz
+  pushd ~/dev_tools
+  tar xvf nvim-linux64.tar.gz
+  popd
+  echo 'alias vim="~/dev_tools/nvim-linux64/bin/nvim"' >> ~/.bashrc
+fi
+
+if [ ! d ~/.config/nvim ]; then
+  mkdir -p ~/.config/nvim
+  mkdir -p ~/.vim
+  cp vim/init.vim ~/.config/nvim
+fi
+
+if [ e ~/.vimrc ]; then
+  cp ~/.vimrc ~/.vimrc.bk
+fi
+cp ~/vim/vimrc ~/.vimrc
+
+if [ e ~/.tmux.conf ]; then
+  cp ~/.tmux.conf ~/.tmux.conf.bk
+fi
+cp ~/tmux/tmux.conf ~/.tmux.conf
+
+echo 'alias tn="tmux new -s"' >> ~/.bashrc
+echo 'alias tl="tmux list-session"' >> ~/.bashrc
+echo 'alias ta="tmux -u -2 a -t"' >> ~/.bashrc
+echo 'alias gs="git status"' >> ~/.bashrc
+echo 'alias gb="git branch"' >> ~/.bashrc
+echo 'alias gd="git diff"' >> ~/.bashrc
