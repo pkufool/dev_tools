1e6185998ae04bc866c0fa6289282fe411570a2f
pkufool <wkang.pku@gmail.com>
2024-03-08 09:30:25 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
-
-
-
 if [ ! -d ~/dev_tools/nvim-linux64 ]; then
   wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-linux64.tar.gz -O ~/dev_tools/nvim-linux64.tar.gz
   pushd ~/dev_tools
-  tar xvf nvim-linux64.tar.gz
+  tar xf nvim-linux64.tar.gz
   popd
   echo 'alias vim="~/dev_tools/nvim-linux64/bin/nvim"' >> ~/.bashrc
 fi
@@ -21,6 +18,8 @@ fi
 cp ./vim/vimrc ~/.vimrc
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+pip install neovim
+sudo apt install universal-ctags
 
 if [ -e ~/.tmux.conf ]; then
   cp ~/.tmux.conf ~/.tmux.conf.bk
@@ -28,6 +27,7 @@ fi
 cp ./tmux/tmux.conf ~/.tmux.conf
 
 echo 'alias tn="tmux new -s"' >> ~/.bashrc
+echo 'alias vim="~/dev_tools/nvim-linux64/bin/nvim"' >> ~/.bashrc
 echo 'alias tl="tmux list-session"' >> ~/.bashrc
 echo 'alias ta="tmux -u -2 a -t"' >> ~/.bashrc
 echo 'alias gs="git status"' >> ~/.bashrc
